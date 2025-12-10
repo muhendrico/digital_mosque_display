ab4533cb72d74135aec32f25d58d816b1467b35e
endrico180 <theendrico@gmail.com>
2025-12-10 09:33:05 +0700

    add migraation and model Article

---
+++ api-service/database/migrations/2025_12_10_022936_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique(); // Untuk URL cantik: /articles/judul-artikel
+            $table->text('image')->nullable(); // URL Gambar
+            $table->text('content'); // Isi lengkap
+            $table->text('summary')->nullable(); // Paragraf pembuka untuk di TV
+            $table->boolean('is_published')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
